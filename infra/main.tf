ad3ef850d8d6cc06274fcd19f66a1b5289036ac8
gabriel <gbp@ic.ufal.br>
2024-08-25 16:47:28 -0300

    Set up EC2 Server on AWS

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_instance" "terraform-example" {
+  ami = "ami-0490fddec0cbeb88b"  # https://us-east-2.console.aws.amazon.com/ec2/home?region=us-east-2#AMICatalog:
+  instance_type = "t2.nano"  # https://aws.amazon.com/ec2/instance-types/
+}
